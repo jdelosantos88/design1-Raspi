508635746e933f759672bc7c410d9ed1bfc6da84
jdelosantos88 <154526861+jdelosantos88@users.noreply.github.com>
2024-03-28 12:59:22 +0800

    Update 1_Real_time_prediction.py
    cv2 cameracapture

--- 4_attendance_webapp/pages/1_Real_time_prediction.py
+++ 4_attendance_webapp/pages/1_Real_time_prediction.py
@@ -27,6 +27,43 @@ realTimepred = face_rec.RealTimePrediction()
 
 # 'streamlit webrtc' for video capture
 
+# 'streamlit webrtc' for video capture
+
+
+# def video_frame_callback(frame):
+#     global setTime
+#     new_width, new_height = 3840, 2160
+
+#     img = frame.to_ndarray(format="bgr24") # 3 dimension numpy array
+#     pred_img = realTimepred.face_prediction(img, redis_face_db,'face_embeddings',
+#                                         ['Name','Course','IDnumber','SPN','GPN'], thresh = 0.5)
+#     higher_resolution_img = cv2.resize(pred_img, (new_width, new_height))
+
+    
+
+#     timenow = time.time()
+#     difftime = timenow - setTime
+#     if difftime >= waitTime: 
+#         realTimepred.save_logs_db()
+#         setTime = time.time()
+#         print('Save Data to database')
+
+#     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+
+# webrtc_streamer(
+#     key="RealtimeAttendance",
+#     video_frame_callback=video_frame_callback,
+#     media_stream_constraints={
+#         "video": {
+#             "width": {"ideal": 2500, "min": 1280},
+#             "height": {"ideal": 1000, "min": 720},
+#         },
+#     }
+# )
+
+
+display_width, display_height = 1280, 720
+
 
 def video_frame_callback(frame):
     global setTime
